fb898aab748c66e6d873a9d43b4af1ad8db7f5b5
fabien <fabien@idolum.info>
2021-05-08 00:12:48 +0200

    Toggle Base/Bépo, with leds
    Base layer is HSV_GOLD, bépo layer is HSV_TEAL

--- keyboards/xbows/nature/keymaps/fabidolum/keymap.c
+++ keyboards/xbows/nature/keymaps/fabidolum/keymap.c
@@ -16,12 +16,10 @@
 #include QMK_KEYBOARD_H
 #include "keymap_extras/keymap_bepo.h"
 
-enum layers {
-    LR_BASE, // default layer
-    LR_BASE_FN, // default mod layer 
-    LR_BEPO, // BÉPO 
-};
-
+#define LR_BASE 0      // almost original layer: qwerty
+#define LR_BEPO 1      // bépo layer with bepo keymap
+//#define LR_ASC_BEPO   // bépo layer with Canadian Multilguage (ASC)
+#define LR_BASE_FN 2   // original fn layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap VANILLA: (Base Layer) Default Layer
@@ -62,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap fabidolum: (Base Layer) Bépo Layer
    *
    * |---------------------------------------------------------------------------------------------------------------------------------|
-   * | Esc |  F1  |  F2  |  F3  |  F4  |      F5  |  F6  |  F7  |  F8  |      F9  |  F10 |  F11 |  F12 |   Delete   |    Toggle QWERTY    |
+   * | Esc |  F1  |  F2  |  F3  |  F4  |      F5  |  F6  |  F7  |  F8  |      F9  |  F10 |  F11 |  F12 |   Delete   |    Toggle BEPO    |
    * |---------------------------------------------------------------------------------------------------------------------------------|
    * |  ~  |     1   |   2   |   3   |   4   |    5      |       6    |    7    |    8   |   9  |   0  |   -  |  =  |  Backspace  |
    * |---------------------------------------------------------------------------------------------------------------------------------|
@@ -77,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [LR_BEPO] = LAYOUT(
-		KC_ESC,  KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  TG(LR_BASE),
-		KC_GRV,  KC_1,    KC_2,  KC_3,   KC_4,   KC_5,             KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,  BP_B,    B_EACU,  KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,  KC_I,   KC_O,    KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-		KC_CAPS, KC_A,    KC_S,  KC_D,   KC_F,   KC_G,    KC_BSPC, KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
-		KC_LSFT, KC_Z,    KC_X,  KC_C,   KC_V,   KC_B,    KC_ENT,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL, KC_LGUI, KC_LALT,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RALT, MO(1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT)
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  TG(LR_BEPO),
+		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,             KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+		KC_TAB,  BP_B,    BP_EACU, BP_P,   BP_O,   BP_EGRV,          BP_W,   BP_V,  BP_D,    BP_L,   BP_J,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+		KC_CAPS, BP_A,    BP_U,    BP_I,   BP_E,   BP_COMM, KC_BSPC, BP_C,   BP_T,  BP_S,    BP_R,   BP_N,    BP_M,    KC_ENT,  KC_PGDN,
+		KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_ENT,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
+		KC_LCTL, KC_LGUI, KC_LALT,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RALT, MO(LR_BASE_FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT)
 };
 
   /* Keymap fabidolum: (Base Layer) Bépo Layer
@@ -124,3 +122,20 @@ moved from bepo : ^ (removed), W, ç, ê
   ÂøÊ±æðÛÎÔ¹²³
   |«»©®ß¬¿×÷¡
   */
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    //switch (get_highest_layer(state)) {
+    switch (biton32(state)){
+    case LR_BEPO:
+        rgb_matrix_sethsv_noeeprom(HSV_TEAL);
+        //rgb_matrix_sethsv_noeeprom(0xF0, 0xF0, 0xF0);
+        break;
+    case LR_BASE_FN:
+        //rgb_matrix_set_color_all (0xFF,  0x00, 0x00);
+        break;
+    default: //  for any other layers, or the default layer
+        rgb_matrix_sethsv(HSV_GOLD);
+        break;
+    }
+  return state;
+}
