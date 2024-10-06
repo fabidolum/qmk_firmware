652dd5b945fe4119ee9ec902621cbfd42c259942
fabien <fabien@idolum.info>
2024-10-06 21:38:58 +0200

    starting with layout ergol

--- keyboards/xbows/nature/keymaps/fabidolum/keymap.c
+++ keyboards/xbows/nature/keymaps/fabidolum/keymap.c
@@ -15,6 +15,10 @@
  */
 #include QMK_KEYBOARD_H
 
+#define LR_VANILLA 0 // orginal qwerty layer
+#define LR_RGB     8 // function layer for RGB
+#define LR_ERGOL_BASE 1 // base layer for ergol
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap VANILLA: (Base Layer) Default Layer
    *
@@ -32,18 +36,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl | GUI |     Alter   |    Space   |   Ctrl   |   Shift   |     Space     |    Alter   |  FN  | Ctrl | Lft  |  Dn |  Rig |
    * |---------------------------------------------------------------------------------------------------------------------------------|
    */
-  [0] = LAYOUT(
-		KC_ESC,  KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  KC_PSCR,
+  [LR_VANILLA] = LAYOUT(
+		KC_ESC,  KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  TG(LR_ERGOL_BASE),
 		KC_GRV,  KC_1,    KC_2,  KC_3,   KC_4,   KC_5,             KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,  KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,  KC_I,   KC_O,    KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
 		KC_CAPS, KC_A,    KC_S,  KC_D,   KC_F,   KC_G,    KC_BSPC, KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
 		KC_LSFT, KC_Z,    KC_X,  KC_C,   KC_V,   KC_B,    KC_ENT,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL, KC_LGUI, KC_LALT,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RALT, MO(1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
-  [1] = LAYOUT(
-    QK_BOOT,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CALC,   KC_MYCM,  KC_MSEL,   KC_MAIL,   NK_TOGG,   EE_CLR,
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NUM,
-    RGB_TOG,   RGB_MOD, RGB_VAI, RGB_HUI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_HOME,
-    KC_TRNS,   RGB_SPD, RGB_VAD, RGB_SPI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_END,
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_MUTE,   KC_VOLU,
-    KC_TRNS,   KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,   KC_TRNS,  KC_MPLY,   KC_MPRV,   KC_VOLD,   KC_MNXT)
+		KC_LCTL, KC_LGUI, KC_LALT,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RALT, MO(LR_RGB),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+  [LR_RGB] = LAYOUT(
+    QK_BOOT,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  KC_CALC,   KC_MYCM,  KC_MSEL,   KC_MAIL,   NK_TOGG,   EE_CLR,
+    _______,   _______, _______, _______,  _______,  _______,             _______,  _______,  _______,   _______,  _______,   _______,   _______,   KC_NUM,
+    RGB_TOG,   RGB_MOD, RGB_VAI, RGB_HUI,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,   KC_HOME,
+    _______,   RGB_SPD, RGB_VAD, RGB_SPI,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,   KC_END,
+    _______,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   KC_MUTE,   KC_VOLU,
+    _______,   _______, _______,           _______,  _______,   _______,  _______,            _______,   _______,  KC_MPLY,   KC_MPRV,   KC_VOLD,   KC_MNXT),
+/* changes:
+* MO(1) => MO(LR_RGB)
+* PrtScr => toggle ergol layer on/off
+*/
+ [LR_ERGOL_BASE] = LAYOUT(
+    _______,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,   _______,
+    _______,   _______, _______, _______,  _______,  _______,             _______,  _______,  _______,   _______,  _______,   _______,   _______,   _______,
+    _______,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,   _______,
+    _______,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,   _______,
+    _______,   _______, _______, _______,  _______,  _______,   _______,  _______,  _______,  _______,   _______,  _______,   _______,   _______,
+    _______,   _______, _______,           _______,  _______,   _______,  _______,            _______,   _______,  _______,   _______,   _______,   _______)
 };
